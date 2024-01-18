86a0ba0914e29363b98e9b6776cca59cb4461ad2
Anastasiia Bulatkina <anstbulatkina@gmail.com>
2024-01-18 14:25:49 +0100

    App is upgraded

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hi")
+from flask import Flask, render_template, url_for, request, Response 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+if __name__ == 'main':
+    app.run(port=5001, debug=True)
